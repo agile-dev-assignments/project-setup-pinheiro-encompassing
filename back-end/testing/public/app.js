336bba1d2671ec135edb3cc7e41f9775b9abfd4f
Adnan Rahat <adnanrahat@Adnans-MacBook-Pro.local>
2021-03-26 13:59:25 -0500

    skeleton implementation of firebase

---
+++ back-end/testing/public/app.js
@@ -0,0 +1,11 @@
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+/// Sign in event handlers
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
