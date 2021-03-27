75df7db85e828102167a8915c8625cb3ce0ab192
karan <kt1757@nyu.edu>
2021-03-27 16:03:45 -0400

    iOS ONLY - does not redirect - need to add security.

---
+++ front-end/app/GoogleAuthentication.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Text } from 'react-native';
+import * as GoogleSignIn from 'expo-google-sign-in';
+import * as Google from 'expo-google-app-auth';
+
+export default class GoogleAuthentication extends React.Component {
+  state = { user: null };
+
+  componentDidMount() {
+    this.initAsync();
+  }
+
+  initAsync = async () => {
+    await GoogleSignIn.initAsync({
+      clientId: '978388863015-4h47p1bsiaim8kktofqk9ssual0as82q.apps.googleusercontent.com',
+    });
+    this._syncUserWithStateAsync();
+  };
+
+  _syncUserWithStateAsync = async () => {
+    const user = await GoogleSignIn.signInSilentlyAsync();
+    this.setState({ user });
+  };
+
+  signOutAsync = async () => {
+    await GoogleSignIn.signOutAsync();
+    this.setState({ user: null });
+  };
+
+  signInAsync = async () => {
+    try {
+      await GoogleSignIn.askForPlayServicesAsync();
+      const { type, user } = await GoogleSignIn.signInAsync();
+      if (type === 'success') {
+        this._syncUserWithStateAsync();
+      }
+    } catch ({ message }) {
+      alert('login: Error:' + message);
+    }
+  };
+
+  onPress = () => {
+    if (this.state.user) {
+      this.signOutAsync();
+    } else {
+      this.signInAsync();
+    }
+  };
+
+  render() {
+    return <Text onPress={this.onPress}>Google Authentication</Text>;
+  }
+}
