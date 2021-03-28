1110188742945644beffe6adc7036d60603e8e27
techsupport-2006 <61287026+techsupport-2006@users.noreply.github.com>
2021-03-28 13:07:00 -0400

    Add files via upload
    -fixed firebase integration

--- front-end/App.js
+++ front-end/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Text, SafeAreaView, StyleSheet } from "react-native";
@@ -5,39 +6,35 @@ import ConnectedDevicesContainer from "./app/ConnectedDevicesContainer";
 import Login from "./app/LoginScreen";
 import ClipboardContainer from "./app/ClipboardContainer";
 import LoginScreen from "./app/LoginScreen";
-//import * as GoogleSignIn from 'expo-google-sign-in';
-import * as Google from 'expo-google-app-auth';
+import Navigation from './app/Navigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as firebase from 'firebase';
 
-async function signInWithGoogleAsync() {
-  try {
-    const result = await Google.logInAsync({
-      androidClientId: "978388863015-7ilvtnu772i66f22vj5t1d3enli35h8u.apps.googleusercontent.com",
-      iosClientId: "978388863015-4h47p1bsiaim8kktofqk9ssual0as82q.apps.googleusercontent.com",
-      scopes: ['profile', 'email'],
-    });
+import { firebaseConfig } from './config';
+firebase.initializeApp(firebaseConfig);
 
-    if (result.type === 'success') {
-      return result.accessToken;
-    } else {
-      return { cancelled: true };
-    }
-  } catch (e) {
-    return { error: true };
-  }
-}
+
+//export default function App() {
+//   let [visible, setVisible] = useState(true);
+//   return visible ? (
+//     <SafeAreaView style={styles.container}>
+//       <ClipboardContainer onPress={() => setVisible(false)} />
+//     </SafeAreaView>
+//   ) : (
+//     <SafeAreaView style={styles.container}>
+//       <LoginScreen onPress={() => setVisible(true)} />
+//     </SafeAreaView>
+//   );
+// }
 
 export default function App() {
-  let [visible, setVisible] = useState(true);
-  return visible ? (
-    <SafeAreaView style={styles.container}>
-      <ClipboardContainer onPress={() => setVisible(false)} />
-    </SafeAreaView>
-  ) : (
-    <SafeAreaView style={styles.container}>
-      <LoginScreen onPress={() => setVisible(true)} />
-    </SafeAreaView>
+  return (
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
