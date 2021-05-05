0cd81de6584a28ecbf50bd42edd4b005e0e601bd
Whitney Dankworth <dwdankworth@gmail.com>
2021-05-05 02:13:46 -0400

    added display box

--- front-end/app/ClipboardList.js
+++ front-end/app/ClipboardList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import db from "./firebase.config.js";
 import firebase from "firebase";
 import Moment from "moment";
-import {userID} from "./LoginScreen.js";
+import { userID } from "./LoginScreen.js";
 import {
   StyleSheet,
   Text,
@@ -15,7 +15,6 @@ import {
 } from "react-native";
 
 var uData = "";
-
 
 const Item = ({ content }) => {
   const writeToClipboard = async () => {
@@ -32,33 +31,47 @@ const Item = ({ content }) => {
 const ClipboardList = () => {
   let [data, loadData] = useState([]);
   let [isLoading, setIsLoading] = useState(true);
+  let [singleLoad, setSingleLoad] = useState(true);
   const [blogs, setBlogs] = useState([]);
   const temp = [];
+  const writeToClipboard = async () => {
+    await Clipboard.setString(data);
+    alert("Copied to Clipboard!");
+  };
+
+  if (singleLoad) {
+    firebase
+      .database()
+      .ref("/users/" + userID)
+      .on("value", function (snapshot) {
+        uData = snapshot.val()["clipboard"];
+        loadData(uData);
+        console.log("udata: " + uData);
+        setSingleLoad(false);
+      });
+  }
 
   console.log("MADE IT HERE");
 
-  firebase.database().ref("/users/"+userID).on("value", function(snapshot) {
-    console.log(snapshot.val()["clipboard"]);
-    uData = snapshot.val()["clipboard"];
-    console.log(uData);
-  });
+  firebase
+    .database()
+    .ref("/users/" + userID)
+    .on("value", function (snapshot) {
+      uData = snapshot.val()["clipboard"];
+      console.log("udata: " + uData);
+    });
 
   const renderItem = ({ item }) => (
     <Item date={item.date} content={item.content} />
   );
-  
-  return isLoading ? (
+
+  return singleLoad ? (
     <ActivityIndicator size="large" style={{ flex: 0.5 }} />
   ) : (
     <SafeAreaView style={{ flex: 1 }}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        style={styles.list}
-        keyExtractor={(item, index) => {
-          return index.toString();
-        }}
-      ></FlatList>
+      <TouchableOpacity style={styles.item} onPress={writeToClipboard}>
+        <Text style={styles.clipboardContent}>{data}</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
